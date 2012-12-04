f2674eaaee3837adadd22c9ce1fd4cac2a6bc141
John Beal <jbeal@MacPro.local>
2012-12-04 10:34:39 -0500

    copy & paste guts of AbdominalFat

--- AbFab01Filter.h
+++ AbFab01Filter.h
@@ -5,13 +5,20 @@
 //  Copyright (c) 2012 John. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
 #import <OsiriXAPI/PluginFilter.h>
 
 @interface AbFab01Filter : PluginFilter {
-
+	NSMutableDictionary *dictMetaData;
 }
 
+@property(readwrite, assign) NSMutableDictionary *dictMetaData;
+
 - (long) filterImage:(NSString*) menuName;
+
+- (NSString*)getDCMValue:(NSString*)aTagName;
+
+- (id) getFunction:(NSString*) anExpressionForFunction :(NSArray*) anExpressionForConstantValue;
 
 @end
